5078bd04e8ae220033b6feaf9cbb6623ca5cf3bc
ZorroKevin <kevindavidzorro@gmail.com>
2026-05-28 21:24:08 -0500

    revertir falla aproposito

--- src/main/java/com/empresa/pedidos/dominio/Pedido.java
+++ src/main/java/com/empresa/pedidos/dominio/Pedido.java
@@ -6,7 +6,6 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 @Entity
 public class Pedido {
